464328ed8733e7eaceb487c4d28e48909be69a3a
rakeshramakrishnan <rakeshramakrishnan92@gmail.com>
2014-04-30 01:08:47 +0530

    Modify Capital case of class attributes

--- Version 1/Swaram.py
+++ Version 1/Swaram.py
@@ -1,24 +1,24 @@
 
 class Swaram(object):
-    def __init__(self, swaram = None, swarasthanam = None):
-        self.swaramname = swaram
-        self.swarasthanam = swarasthanam
+    def __init__(self, swaram = None, Swarasthanam = None):
+        self.SwaramName = swaram
+        self.Swarasthanam = Swarasthanam
      
-    def SetSwaram(self, swaramname, swarasthanam):
-        self.swaramname = swaramname
-        self.swarasthanam = swarasthanam
+    def SetSwaram(self, SwaramName, Swarasthanam):
+        self.SwaramName = SwaramName
+        self.Swarasthanam = Swarasthanam
     
-    def SetSwarasthanam(self, swarasthanam):
-        self.swarasthanam = swarasthanam
+    def SetSwarasthanam(self, Swarasthanam):
+        self.Swarasthanam = Swarasthanam
     
     def GetSwaram(self):
         swaramtemp = []
-        swaramtemp.append(self.swaramname)
-        swaramtemp.append(self.swarasthanam)
+        swaramtemp.append(self.SwaramName)
+        swaramtemp.append(self.Swarasthanam)
         return swaramtemp
     
     def GetSwarasthanam(self):
-        return self.swarasthanam
+        return self.Swarasthanam
     
     def GetSwaramName(self):
-        return self.swaramname
+        return self.SwaramName
